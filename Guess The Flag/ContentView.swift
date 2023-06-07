dc2afb0feb3ed408308bd426384d1467d9e6fb4f
Jaden Lee <jaden.lee101@gmail.com>
2023-06-06 20:56:15 -0600

    first commit

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia","France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correct = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.gray
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Pick the country flag :")
+                Text(countries[correct])
+                
+                
+                ForEach(0..<3) { country in
+                    Button{
+                        tapped(country: country)
+                    } label:{
+                        Image(countries[country])
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    private func tapped (country : Int) {
+        if country == correct {
+            print ("correct")
+        } else {
+            print ("not correct")
+        }
     }
 }
 
